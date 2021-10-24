87fc05ae3614a39f3def72cb7ba6a9e8f7a4f2d0
Serge Hänni <serge@nyi.ch>
2021-10-24 20:47:21 +0200

    Change pages to support subdirectories

--- app/controllers/metus/pages_controller.rb
+++ app/controllers/metus/pages_controller.rb
@@ -2,10 +2,30 @@
 
 module Metus
   class PagesController < PublicController
-    PAGES = Dir[Rails.root.join('app/views/pages/*')].map { |filename| File.basename(filename, '.html.haml') }.map(&:to_sym)
+    def self.filename_to_pagename(filename)
+      directory = File.dirname(filename)
+      directory.gsub!(/#{PAGES_DIRECTORY}[\/]?/, '')
+
+      name = File.basename(filename, '.html.haml')
+
+      [directory, name].select(&:present?).join('/').to_sym
+    end
+
+    def self.path_to_actionname(path)
+      path.to_s.gsub('/', '__slash__')
+    end
+
+    PAGES_DIRECTORY = Rails.root.join('app/views/pages')
+
+    PAGES = Dir[PAGES_DIRECTORY.join('**/*')]
+      .reject { |filename| File.directory?(filename) }
+      .map { |filename| Metus::PagesController.filename_to_pagename(filename) }
+      .map(&:to_sym)
 
     PAGES.each do |page|
-      define_method(page) {}
+      define_method(Metus::PagesController.path_to_actionname(page)) do
+        render "pages/#{page}.html.haml"
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Metus::Engine.routes.draw do
   Metus::PagesController::PAGES.each do |page|
-    get "/pages/#{page}", controller: :pages, action: page
+    get "/pages/#{page}", controller: :pages, action: Metus::PagesController.path_to_actionname(page), as: page
   end
 
   namespace :backend do
