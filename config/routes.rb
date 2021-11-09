805d60b75dc482df06cf749e5d3f4b05baf70243
Serge Hänni <serge@nyi.ch>
2021-11-09 14:02:09 +0100

    Add locale scoped routes in the engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Metus::Engine.routes.draw do
-  Metus::PagesController::PAGES.each do |page|
-    get "/pages/#{page}", controller: :pages, action: Metus::PagesController.path_to_actionname(page), as: page
-  end
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    Metus::PagesController::PAGES.each do |page|
+      get "/pages/#{page}", controller: :pages, action: Metus::PagesController.path_to_actionname(page), as: page
+    end
 
-  namespace :backend do
-    get :welcome, to: 'welcome#show'
-    resources :admins, except: :show
+    namespace :backend do
+      get :welcome, to: 'welcome#show'
+      resources :admins, except: :show
+    end
   end
 end
